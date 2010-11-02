18bf70da531fdb14a4387114c2106d9dd1df409b
Jordan Bach <jordanbach@gmail.com>
2010-11-01 23:44:58 -0500

    added a --path option to the generator to install nested_form.js in a custom location

--- lib/generators/nested_form/install_generator.rb
+++ lib/generators/nested_form/install_generator.rb
@@ -1,13 +1,26 @@
+require 'ftools'
+
 module NestedForm
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      desc 'Places a copy of the nested_form.js file in your project'
+      class_option :path,
+                   :type => :string,
+                   :default => 'public/javascripts/nested_form.js',
+                   :desc => 'Specify a custom path for nested_form.js'
+
       def self.source_root
         File.dirname(__FILE__) + "/templates"
       end
       
       
       def copy_jquery_file
-        copy_file 'nested_form.js', 'public/javascripts/nested_form.js'
+        path = options[:path]
+        unless path =~ /\.js$/
+          path = File.join(path, 'nested_form.js')
+        end
+        File.makedirs(File.dirname(path))
+        copy_file 'nested_form.js', path
       end
     end
   end
